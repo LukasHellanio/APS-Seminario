2cbb4ff934f22252cc98faf06fee2db96f558907
Pedro Gabriel Lima <pgsl@ecomp.poli.br>
2022-06-02 03:08:20 -0300

    Singleton Java

--- Pedro - Singleton/DemoSingleThread.java
+++ Pedro - Singleton/DemoSingleThread.java
@@ -4,22 +4,36 @@ public class DemoSingleThread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Os valores resultantes da operacao abaixo serao 'FOO' pois apenas"
+		System.out.println("Os valores resultantes da operacao abaixo serao 'FOO' pois apenas "
 				+ "um objeto foi criado" + "\n");
+		//Aqui vai criar o objeto Singleton pois no momento em que o metodo getInstancia checar
+		// se o objeto instancia da classe Singleton é nulo, ele vai retornar true e a partir daí
+		//vai chamar o construtor privado da classe instanciar um objeto do tipo Singleton
 		Singleton primeiraInstancia = Singleton.getinstancia("FOO");
+		//Quando o cliente cria uma segunda vez um objeto Singleton a partir do metodo getInstnacia
+		// O metodo durante a checagem do objeto instancia vai retornar false, pois na linha 12 nos
+		// o objeto "primeiraInstancia" foi o primeiro a chamar o metodo getInstancia e consequentemente
+		// o primerio a criar o objeto Singleton, e por isso, tudo que chamar o singleton daqui em diante
+		//será apenas o objeto "primeira instancia" sendo manipulado.
 		Singleton segundaInstancia = Singleton.getinstancia("BAR");
-		System.out.println("Primeira instancia:'FOO'");
-        System.out.println("Valor do primeiro objeto instanciado: "+primeiraInstancia.valorQualquer);
-        System.out.println("Segunda instancia: 'BAR'");
-        System.out.println("Valor do segundo objeto instanciado: "+segundaInstancia.valorQualquer + "\n");
-        System.out.println("Agora temos uma modificacao no objeto, em uma das instancias "
-        		+ "e com isso, todas as instancias serao alteradas \n");
+		System.out.println("Valor passado para o objeto primeiraInstancia:'FOO'"+"\n");
+        System.out.println("Valor setado no objeto primeiraInstancia: "+primeiraInstancia.valorQualquer+"\n");
+        System.out.println("=============================================================================\n");
+        System.out.println("Valor passado para o objeto segundaInstancia: 'BAR'"+"\n");
+        System.out.println("Valor setado no objeto segundaInstancia: "+segundaInstancia.valorQualquer + "\n");
+        System.out.println("=============================================================================\n");
+        System.out.println("Agora temos uma modificacao no objeto, em uma das instancias, "
+        		+ " e com isso todas as instancias serao alteradas \n");
+        System.out.println("------------------------------------------------------------------------ \n");
         primeiraInstancia.valorQualquer = "BOO";
-        System.out.println("Nova modificacao no objeto: 'BOO'\n");
-        System.out.println("Primeira instancia antiga: 'FOO'");
-        System.out.println("Novo valor do primeiro obejto: "+primeiraInstancia.valorQualquer+"\n");
-        System.out.println("Segunda instancia antiga: 'FOO'");
-        System.out.println("Novo valor do segundo obejto: "+segundaInstancia.valorQualquer);
+        System.out.println("Novo valor modificado no objeto primeiraInstancia: "+primeiraInstancia.valorQualquer+"\n");
+        System.out.println("------------------------------------------------------------------------ \n");
+        System.out.println("Valor que estava presente anteriormente no objeto primeiraInstancia: 'FOO'" + "\n");
+        System.out.println("Novo valor setado no objeto primeiraInstancia: "+primeiraInstancia.valorQualquer+"\n");
+        System.out.println("------------------------------------------------------------------------ \n");
+        System.out.println("Valor que estava presente anteriormente no objeto segundaInstancia: 'FOO'"+ "\n");
+        System.out.println("Novo valor do objeto segundaInstancia: "+segundaInstancia.valorQualquer + "\n");
+        System.out.println("------------------------------------------------------------------------ \n");
 	}
 
 }
